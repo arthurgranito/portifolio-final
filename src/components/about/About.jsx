84ec48b905eb7b7d327ecc87361bed359c1b08ab
Arthur Granito <arthurcursotecnico@gmail.com>
2024-11-05 15:05:18 -0300

    commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/foto3.jpg";
-import CV from "../../assets/John-Cv.pdf";
+import CV from "../../assets/arthur_cv.pdf";
 import Info from "./Info";
 
 const About = () => {
